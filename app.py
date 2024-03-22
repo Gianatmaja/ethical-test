ca69292717b7b0b280fa5b2302c0217da8d450ca
Gianatmaja <gianatmaja@outlook.com>
2024-03-22 19:47:50 +0800

    Added foodexp and delete

--- app.py
+++ app.py
@@ -34,6 +34,21 @@ def main():
     return render_template("main.html", name = name)
 
 
+@app.route("/delete", methods = ["GET", "POST"])
+def delete():
+
+    conn = sqlite3.connect("log.db")
+    c = conn.cursor()
+    
+    c.execute("delete from employee;")
+    conn.commit()
+    
+    c.close()
+    conn.close()
+        
+    return render_template("delete.html", name = name)
+
+
 @app.route("/ethical_test", methods = ["GET", "POST"])
 def ethical_test():
     return render_template("ethical_test.html")
@@ -54,6 +69,19 @@ def query():
     conn.close()
     
     return render_template("query.html", r = r)
+
+
+@app.route("/food_exp", methods = ["GET", "POST"])
+def food_exp():
+    
+    return render_template("food_exp.html")
+
+
+@app.route("/prediction", methods = ["GET", "POST"])
+def prediction():
+    income = float(request.form.get("income"))
+    return render_template("prediction.html", r = (income * 0.485) + 147)
+
 
 
 @app.route("/answer", methods = ["GET", "POST"])
